0ea2130bcb48ba1815b41e8838e07afdb5c6a8d3
HoYeong Sung <seonghoyeong@seonghoyeong-ui-MacBookPro.local>
2025-04-06 22:20:26 +0900

    1차 폴더구조 셋팅 완료

---
+++ src/pages/login/LoginPage.js
@@ -0,0 +1,13 @@
+// src/pages/login/LoginPage.js
+import React from "react";
+
+function LoginPage() {
+  return (
+    <div style={{ padding: "2rem" }}>
+      <h1>🔐 로그인 페이지</h1>
+      <p>여기서 이메일/비밀번호를 입력하세요.</p>
+    </div>
+  );
+}
+
+export default LoginPage;

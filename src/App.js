e64ad77eb14b55e967976d69c92daae3fc625e0c
Mario <mario322@naver.com>
2025-04-15 21:03:16 +0900

    feat: Navbar와 Sidebar 디자인 개선 및 코드 정리

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
 // src/App.js
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import styled from "styled-components";
 import MainPage from "./pages/main/MainPage";
 import LoginPage from "./pages/login/LoginPage";
 import StudentListPage from "./pages/students/StudentListPage";
+import Navbar from "./components/navigation/Navbar";
+import Sidebar from "./components/navigation/Sidebar";
+
+const AppContainer = styled.div`
+  display: flex;
+  min-height: 100vh;
+`;
+
+const MainContent = styled.main`
+  flex: 1;
+  margin-left: 240px;
+  margin-top: 60px;
+  padding: 2rem;
+  background-color: #f5f5f5;
+`;
 
 function App() {
   return (
-
     <Router>
-      <div style={{ padding: "1rem", background: "#eee" }}>
-        <Link to="/">메인</Link> | <Link to="/login">로그인</Link> |{" "}
-        <Link to="/students">학생 목록</Link>
-      </div>
-      <Routes>
-        <Route path="/" element={<MainPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/students" element={<StudentListPage />} />
-      </Routes>
+      <AppContainer>
+        <Navbar />
+        <Sidebar />
+        <MainContent>
+          <Routes>
+            <Route path="/" element={<MainPage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/students" element={<StudentListPage />} />
+          </Routes>
+        </MainContent>
+      </AppContainer>
     </Router>
-
   );
 }
 

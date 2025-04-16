42120d058f7c9e1396908a285db705324a6a2495
HoYeong Sung <seonghoyeong@seonghoyeong-ui-MacBookPro.local>
2025-04-16 14:38:30 +0900

    StudentBar 컴포넌트 dummyData 구현

---
+++ src/data/dummyStudentBarData.js
@@ -0,0 +1,9 @@
+export const roles = ["teacher", "student", "parent"];
+
+export const classOptions = {
+  "1학년": ["1반", "2반", "3반", "4반", "5반", "6반", "7반"],
+  "2학년": ["1반", "2반", "3반", "4반", "5반"],
+  "3학년": ["1반", "2반", "3반", "4반"],
+};
+
+export const grades = Object.keys(classOptions);
